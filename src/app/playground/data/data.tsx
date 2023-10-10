3807c2b6861f594ad2b9d843ac607b4d4411a34d
au2nd <chotnt741@gmail.com>
2023-10-10 21:17:16 +0900

    fix error message

--- src/app/playground/data/data.tsx
+++ src/app/playground/data/data.tsx
@@ -2,35 +2,39 @@ import { z } from "zod";
 
 export const soilSchema = z.object({
   id: z.string().optional(),
-  토층명칭: z.string(),
+  토층명칭: z
+    .string({
+      invalid_type_error: "토층명칭은 문자열이어야 합니다.",
+    })
+    .optional(),
   굴착깊이: z.preprocess(
     (args) => (args === "" ? undefined : args),
     z.coerce
       .number({
-        invalid_type_error: "초기축력는 숫자여야 합니다.",
+        invalid_type_error: "굴착깊이는 숫자여야 합니다.",
       })
       .positive({
-        message: "초기축력는 양수여야 합니다.",
+        message: "굴착깊이는 양수여야 합니다.",
       })
   ),
   벽체설치: z.preprocess(
     (args) => (args === "" ? undefined : args),
     z.coerce
       .number({
-        invalid_type_error: "초기축력는 숫자여야 합니다.",
+        invalid_type_error: "벽체설치는 숫자여야 합니다.",
       })
       .positive({
-        message: "초기축력는 양수여야 합니다.",
+        message: "벽체설치는 양수여야 합니다.",
       })
   ),
   앵커설치: z.preprocess(
     (args) => (args === "" ? undefined : args),
     z.coerce
       .number({
-        invalid_type_error: "초기축력는 숫자여야 합니다.",
+        invalid_type_error: "앵커설치는 숫자여야 합니다.",
       })
       .positive({
-        message: "초기축력는 양수여야 합니다.",
+        message: "앵커설치는 양수여야 합니다.",
       })
   ),
 });
@@ -43,20 +47,20 @@ export const excavationSchema = z.object({
     (args) => (args === "" ? undefined : args),
     z.coerce
       .number({
-        invalid_type_error: "초기축력는 숫자여야 합니다.",
+        invalid_type_error: "시작심도는 숫자여야 합니다.",
       })
       .positive({
-        message: "초기축력는 양수여야 합니다.",
+        message: "시작심도는 양수여야 합니다.",
       })
   ),
   바닥심도: z.preprocess(
     (args) => (args === "" ? undefined : args),
     z.coerce
       .number({
-        invalid_type_error: "초기축력는 숫자여야 합니다.",
+        invalid_type_error: "바닥심도는 숫자여야 합니다.",
       })
       .positive({
-        message: "초기축력는 양수여야 합니다.",
+        message: "바닥심도는 양수여야 합니다.",
       })
   ),
 });
@@ -69,30 +73,30 @@ export const wallSchema = z.object({
     (args) => (args === "" ? undefined : args),
     z.coerce
       .number({
-        invalid_type_error: "초기축력는 숫자여야 합니다.",
+        invalid_type_error: "단면적은 숫자여야 합니다.",
       })
       .positive({
-        message: "초기축력는 양수여야 합니다.",
+        message: "단면적은 양수여야 합니다.",
       })
   ),
   단면2차모멘트: z.preprocess(
     (args) => (args === "" ? undefined : args),
     z.coerce
       .number({
-        invalid_type_error: "초기축력는 숫자여야 합니다.",
+        invalid_type_error: "단면2차모멘트는 숫자여야 합니다.",
       })
       .positive({
-        message: "초기축력는 양수여야 합니다.",
+        message: "단면2차모멘트는 양수여야 합니다.",
       })
   ),
   탄성계수: z.preprocess(
     (args) => (args === "" ? undefined : args),
     z.coerce
       .number({
-        invalid_type_error: "초기축력는 숫자여야 합니다.",
+        invalid_type_error: "탄성계수는 숫자여야 합니다.",
       })
       .positive({
-        message: "초기축력는 양수여야 합니다.",
+        message: "탄성계수는 양수여야 합니다.",
       })
   ),
 });
@@ -108,30 +112,30 @@ export const anchorSchema = z.object({
     (args) => (args === "" ? undefined : args),
     z.coerce
       .number({
-        invalid_type_error: "초기축력는 숫자여야 합니다.",
+        invalid_type_error: "단면적은 숫자여야 합니다.",
       })
       .positive({
-        message: "초기축력는 양수여야 합니다.",
+        message: "단면적은 양수여야 합니다.",
       })
   ),
   탄성계수: z.preprocess(
     (args) => (args === "" ? undefined : args),
     z.coerce
       .number({
-        invalid_type_error: "초기축력는 숫자여야 합니다.",
+        invalid_type_error: "탄성계수는 숫자여야 합니다.",
       })
       .positive({
-        message: "초기축력는 양수여야 합니다.",
+        message: "탄성계수는 양수여야 합니다.",
       })
   ),
   설치길이: z.preprocess(
     (args) => (args === "" ? undefined : args),
     z.coerce
       .number({
-        invalid_type_error: "초기축력는 숫자여야 합니다.",
+        invalid_type_error: "설치길이는 숫자여야 합니다.",
       })
       .positive({
-        message: "초기축력는 양수여야 합니다.",
+        message: "설치길이는 양수여야 합니다.",
       })
   ),
   초기축력: z.preprocess(
